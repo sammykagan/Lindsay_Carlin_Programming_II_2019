6e12d05ba0882a0c0e901a25dc6636fbda695a92
lindsaycarlin <lcarlin@fwparker.org>
2019-02-11 10:39:56 -0600

    hangman -- work in progress

--- hangman.py
+++ hangman.py
@@ -2,6 +2,7 @@
 Make a text based version of hangman (25pts)
 Use the sample run as an example.  Try to make it as close as possible to the example. (or better)
 '''
+import random
 
 # PSEUDOCODE
 # make a word list for your game
@@ -16,7 +17,6 @@ Use the sample run as an example.  Try to make it as close as possible to the ex
 # if the incorrect_guesses is greater than 6, tell the user they lost and exit the program
 # if correct_guesses is equal to the length of the word, tell the user they won
 # ask if they want to play again
-
 
 # Feel free to use this list of ascii art for your game
 
@@ -72,14 +72,38 @@ HANGMANPICS = ['''
 =========''']
 
 
+word_list = ["Sammy", "Nicky", "Ethan", "Benji", "Lindsay", "MrLee"]
+word = word_list[random.randrange(5)]
+used_letters = []
+correct_guesses = 0
+guessed_letters = []
+incorrect_guesses = 0
+hangman_pic = 0
+print(HANGMANPICS[hangman_pic])
+for i in range(len(word)):  # need to be all on same line
+    print("_ ", end = "")
+for i in range(27):
+    guess = input("\nYour guess: ")
+    if guess in guessed_letters:
+        print("You already guessed that, silly!")
+    guessed_letters.append(guess)
+    if guess in word:
+        correct_guesses += 1
+        print([guess])
+    else:
+        incorrect_guesses += 1
+        hangman_pic += 1
+        print(HANGMANPICS[hangman_pic])
+    if incorrect_guesses > 6:
+        print("You lost!")
+        # end program
+    if correct_guesses == len(word):
+        print("You won! Want to play again?")
 
 
-
-
-
-
-
-
-
+# if the guess is correct, add it to the word in the corresponding blank
+# if the incorrect_guesses is greater than 6, tell the user they lost and exit the program
+# if correct_guesses is equal to the length of the word, tell the user they won
+# ask if they want to play again
 
 
